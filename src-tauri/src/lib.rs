d6d92c0460c86d692d36959a0ecbc8a966320597
Felipe Valtl de Mello <valtlfelipe@gmail.com>
2025-08-13 14:39:26 -0300

    feat: refactor main function to streamline application setup

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,48 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{TitleBarStyle, WebviewUrl, WebviewWindowBuilder};
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .setup(|app| {
+            let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+                .title("Hedit")
+                .min_inner_size(1000.0, 650.0);
+
+            // set transparent title bar only when building for macOS
+            #[cfg(target_os = "macos")]
+            let win_builder = win_builder
+                .hidden_title(true)
+                .title_bar_style(TitleBarStyle::Overlay);
+
+            #[cfg(not(target_os = "macos"))]
+            let win_builder = win_builder.decorations(false);
+
+            let window = win_builder.build().unwrap();
+
+            // set background color only when building for macOS
+            #[cfg(target_os = "macos")]
+            {
+                use cocoa::appkit::{NSColor, NSWindow};
+                use cocoa::base::{id, nil};
+
+                let ns_window = window.ns_window().unwrap() as id;
+                unsafe {
+                    let bg_color = NSColor::colorWithRed_green_blue_alpha_(
+                        nil,
+                        50.0 / 255.0,
+                        158.0 / 255.0,
+                        163.5 / 255.0,
+                        1.0,
+                    );
+                    ns_window.setBackgroundColor_(bg_color);
+                }
+            }
+
+            Ok(())
+        })
+        .plugin(tauri_plugin_fs::init())
+        // .invoke_handler(tauri::generate_handler![read_hosts_file, write_hosts_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,51 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{TitleBarStyle, WebviewUrl, WebviewWindowBuilder};
-
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_store::Builder::new().build())
-        .setup(|app| {
-            let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
-                .title("Hedit")
-                .min_inner_size(1000.0, 650.0);
-
-            // set transparent title bar only when building for macOS
-            #[cfg(target_os = "macos")]
-            let win_builder = win_builder
-                .hidden_title(true)
-                .title_bar_style(TitleBarStyle::Overlay);
-
-            #[cfg(not(target_os = "macos"))]
-            let win_builder = win_builder.decorations(false);
-
-            let window = win_builder.build().unwrap();
-
-            // set background color only when building for macOS
-            #[cfg(target_os = "macos")]
-            {
-                use cocoa::appkit::{NSColor, NSWindow};
-                use cocoa::base::{id, nil};
-
-                let ns_window = window.ns_window().unwrap() as id;
-                unsafe {
-                    let bg_color = NSColor::colorWithRed_green_blue_alpha_(
-                        nil,
-                        50.0 / 255.0,
-                        158.0 / 255.0,
-                        163.5 / 255.0,
-                        1.0,
-                    );
-                    ns_window.setBackgroundColor_(bg_color);
-                }
-            }
-
-            Ok(())
-        })
-        .plugin(tauri_plugin_fs::init())
-        // .invoke_handler(tauri::generate_handler![read_hosts_file, write_hosts_file])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    hedit_app_lib::run();
 }
