532f44e5f1adbfe71976608af0c24866a80f364d
Felipe Valtl de Mello <valtlfelipe@gmail.com>
2025-12-10 14:20:34 -0300

    chore: fix missing format on unchanged file

--- src/composables/useTelemetry.ts
+++ src/composables/useTelemetry.ts
@@ -3,14 +3,14 @@
 // Telemetry can be disabled by the user
 // See src-tauri/src/telemetry.rs for more details
 
-import { invoke } from "@tauri-apps/api/core"
+import { invoke } from '@tauri-apps/api/core'
 
 export function useTelemetry() {
-    const trackEvent = (eventName: string) => {
-        return invoke('send_telemetry_event', { event: eventName })
-    }
+  const trackEvent = (eventName: string) => {
+    return invoke('send_telemetry_event', { event: eventName })
+  }
 
-    return {
-        trackEvent,
-    }
+  return {
+    trackEvent,
+  }
 }
